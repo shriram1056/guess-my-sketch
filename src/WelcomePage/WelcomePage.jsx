3df7fdde3c975af23a0266c977b99aabe94d4798
shriram1056 <saravananshriram80@gmail.com>
2023-09-17 19:31:30 -0300

    add a popup for create room

--- src/WelcomePage/WelcomePage.jsx
+++ src/WelcomePage/WelcomePage.jsx
@@ -1,15 +1,26 @@
+import { Button, Modal, Form } from 'react-bootstrap'
 import React from 'react'
 import { useState } from 'react'
 import './WelcomePage.css'
 
 export function WelcomePage() {
   const [showCreateRoomModal, setShowCreateRoomModal] = useState(false)
+  const [formData, setFormData] = useState({}) // State for form data
 
   const handleNewRoom = () => {
     setShowCreateRoomModal(true) // Show the modal when "Create Room" is clicked
   }
   const handleJoinRoom = () => {
     setShowCreateRoomModal(true) // Show the modal when "Create Room" is clicked
+  }
+
+  const handleCreateRoomCloseModal = () => {
+    setShowCreateRoomModal(false) // Close the modal
+  }
+
+  const handleCreateRoomSubmit = () => {
+    console.log(formData)
+    handleCreateRoomCloseModal()
   }
 
   return (
@@ -30,6 +41,34 @@ export function WelcomePage() {
           </button>
         </div>
       </div>
+      <Modal
+        show={showCreateRoomModal}
+        onHide={handleCreateRoomCloseModal}
+        backdrop="static"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Create a Room</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="userName">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter user name"
+                onChange={(e) =>
+                  setFormData({ ...formData, userName: e.target.value })
+                }
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCreateRoomSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
