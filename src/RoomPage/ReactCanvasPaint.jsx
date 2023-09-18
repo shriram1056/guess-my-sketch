ee5fe45db79496a78f2b1d503eea778855bb965f
shriram1056 <saravananshriram80@gmail.com>
2023-09-18 00:11:33 -0300

    canvas ready

---
+++ src/RoomPage/ReactCanvasPaint.jsx
@@ -0,0 +1,173 @@
+import React, { useRef, useState, useCallback, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import styles from './styles.module.css'
+
+// Import an eraser icon from a library or use an image
+import EraserIcon from '../assets/svg/eraser.svg' // Replace with your actual eraser icon source
+import { viewport } from '@popperjs/core'
+
+function DrawingToolBox({ colors, active, onChange }) {
+  const handleColorClick = (color) => {
+    onChange(color)
+  }
+
+  return (
+    <div className={styles.toolBoxContainer}>
+      <div className={styles.colors}>
+        {colors.map((color, key) => (
+          <button
+            key={key}
+            onClick={() => handleColorClick(color)}
+            className={classNames(styles.color, {
+              [styles.active]: active === color,
+            })}
+            style={{
+              backgroundColor: color,
+              pointerEvents: 'auto', // Allow color selection even when eraser is active
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function ReactCanvasPaint(props) {
+  const canvas = useRef(null)
+  const [drawing, setDrawing] = useState(false)
+  const [position, setPosition] = useState(null)
+  const [activeColor, setActiveColor] = useState(props.colors[0])
+
+  const onDown = useCallback((event) => {
+    const coordinates = getCoordinates(event)
+    if (coordinates) {
+      setPosition(coordinates)
+      setDrawing(true)
+    }
+  }, [])
+
+  const onUp = useCallback(() => {
+    setDrawing(false)
+    setPosition(null)
+  }, [])
+
+  const getCoordinates = (event) => {
+    if (!canvas.current) {
+      return null
+    }
+
+    const canvasRect = canvas.current.getBoundingClientRect()
+    const x = event.clientX - canvasRect.left
+    const y = event.clientY - canvasRect.top
+
+    return {
+      x,
+      y,
+    }
+  }
+
+  const onMove = useCallback(
+    (event) => {
+      if (drawing) {
+        const newPosition = getCoordinates(event)
+        if (position && newPosition) {
+          drawLine(position, newPosition)
+          setPosition(newPosition)
+        }
+      }
+    },
+    [drawing, position]
+  )
+
+  const drawLine = (originalPosition, newPosition) => {
+    if (!canvas.current) {
+      return null
+    }
+
+    const context = canvas.current.getContext('2d')
+
+    if (context) {
+      context.strokeStyle = activeColor
+      context.lineJoin = 'round'
+      context.lineWidth = props.strokeWidth
+
+      context.beginPath()
+      context.moveTo(originalPosition.x, originalPosition.y)
+      context.lineTo(newPosition.x, newPosition.y)
+      context.closePath()
+
+      context.stroke()
+      handleDraw(context.getImageData(0, 0, props.width, props.height))
+    }
+  }
+
+  const handleDraw = (data) => {
+    if (typeof props.onDraw === 'function') {
+      props.onDraw(data)
+    }
+  }
+
+  useEffect(() => {
+    if (typeof props.data === 'object' && canvas.current) {
+      const context = canvas.current.getContext('2d')
+      // TODO: scale imageData
+      context.putImageData(props.data, 0, 0)
+    }
+  }, [props.data])
+
+  return (
+    <div className={styles.container}>
+      {/* Add a button or icon to toggle eraser */}
+      {!props.viewOnly && (
+        <button
+          className={styles.eraserButton}
+          onClick={() => setActiveColor('#ffffff')}
+        >
+          <img src={EraserIcon} alt="Eraser" className={styles.eraserIcon} />
+        </button>
+      )}
+      <canvas
+        ref={canvas}
+        onMouseDown={props.viewOnly ? undefined : onDown}
+        onTouchStart={props.viewOnly ? undefined : onDown}
+        onMouseUp={props.viewOnly ? undefined : onUp}
+        onTouchEnd={props.viewOnly ? undefined : onUp}
+        onMouseLeave={props.viewOnly ? undefined : onUp}
+        onMouseMove={props.viewOnly ? undefined : onMove}
+        onTouchMove={props.viewOnly ? undefined : onMove}
+        width={props.width}
+        height={props.height}
+      />
+      {!props.viewOnly && (
+        <DrawingToolBox
+          colors={props.colors}
+          active={activeColor}
+          onChange={setActiveColor}
+        />
+      )}
+    </div>
+  )
+}
+
+ReactCanvasPaint.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  viewOnly: PropTypes.bool,
+  data: PropTypes.object,
+  onDraw: PropTypes.func,
+  colors: PropTypes.arrayOf(PropTypes.string),
+  strokeWidth: PropTypes.number,
+}
+
+ReactCanvasPaint.defaultProps = {
+  width: 400,
+  height: 400,
+  viewOnly: false,
+  data: undefined,
+  onDraw: undefined,
+  colors: ['#7030A2', '#000000', '#0170C1', '#FE0002', '#FFFF01', '#00AF52'],
+  strokeWidth: 5,
+}
+
+export default ReactCanvasPaint
--- src/RoomPage/RoomPage.jsx
+++ src/RoomPage/RoomPage.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from 'react'
+import ReactCanvasPaint from './ReactCanvasPaint'
 import './RoomPage.css'
 
 export function RoomPage() {
   const [draw, setDraw] = useState(undefined)
+  console.log(draw)
   return (
-    <div className="welcome-page d-flex flex-column justify-content-center align-items-center vh-100"></div>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <div className="canvas">
+        <ReactCanvasPaint
+          strokeWidth={15}
+          width={400}
+          height={400}
+          onDraw={setDraw}
+        />
+      </div>
+      <div className="canvas">
+        <ReactCanvasPaint
+          viewOnly
+          strokeWidth={15}
+          width={400}
+          height={400}
+          data={draw}
+        />
+      </div>
+    </div>
   )
 }
 
